2e335452bd94bbec7224f27f64c95b117a03c6d3
FreddyKilo <freddy.kilo@gmail.com>
2015-10-21 19:24:37 -0700

    add versatility

--- largestPalindromeProduct/LargestPalindromProduct.java
+++ largestPalindromeProduct/LargestPalindromProduct.java
@@ -3,14 +3,16 @@ package com.freddy.kilo.largestPalindromeProduct;
 public class LargestPalindromProduct {
 
 	public static void main(String[] args) {
-		System.out.println(getLargestPalindrome());
+		System.out.println(getLargestPalindrome(3));
 	}
 
-	public static int getLargestPalindrome(){
+	public static int getLargestPalindrome(int digits){
+		int upper = (int) Math.pow(10, digits);
+		int lower = (int) Math.pow(10, digits - 1);
 		String productStr;
-		int candidate = 100 * 100;
-		for(int i = 100; i<999; i++){
-			for(int j = 100; j<999; j++){
+		int candidate = 0;
+		for(int i = lower; i<upper; i++){
+			for(int j = lower; j<upper; j++){
 				productStr = String.valueOf(i*j);
 				if(productStr.equals(new StringBuilder(productStr).reverse().toString())){
 					if(i * j > candidate)
